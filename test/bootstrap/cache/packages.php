38ad58a0ebdeeb64b2dd05c7268208f4645eb103
Ziad <98745109+zyn-code@users.noreply.github.com>
2025-06-19 10:22:46 +0300

    Add files via upload

---
+++ test/bootstrap/cache/packages.php
@@ -0,0 +1,23 @@
+<?php return array (
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
