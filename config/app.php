9dc5276fb816d606bda7292e764c892009e01bc0
Ziad <98745109+zyn-code@users.noreply.github.com>
2025-06-19 11:49:36 +0300

    Add files via upload

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://handover-beeecmfxarh2h8be.canadacentral-01.azurewebsites.net'),
 
     /*
     |--------------------------------------------------------------------------
